292caf35449468d8ecedecbefcf5baaff53264d7
Repnikov.A <repnikov.a@florcom.ru>
2023-08-10 11:59:09 +0300

    Добавьте файлы проекта.

---
+++ HW_8_6_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using HW_8_6_1;
+
+
+class Program
+{
+    public static void Main(string[]  args)
+    {
+        Console.WriteLine("Введите путь к каталогу");
+        string path = Console.ReadLine();
+    }
+}
